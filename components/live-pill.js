5f87c64c7c682684c59d8acdd55e5b8d5f5a4736
Tofik Khan <tofik.khan@mkausa.org>
2023-11-07 23:27:24 -0800

    Create Pill component for a live indicator

---
+++ components/live-pill.js
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+import { CheckCircle } from "react-bootstrap-icons";
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+  padding: 4px 8px;
+  border: 3px solid red;
+  border-radius: 15px;
+  width: fit-content;
+  color: red;
+
+  -webkit-animation: pulsate-fwd 1s ease-in-out infinite both;
+  animation: pulsate-fwd 1s ease-in-out infinite both;
+
+  @-webkit-keyframes pulsate-fwd {
+    0% {
+      -webkit-transform: scale(1);
+      transform: scale(1);
+    }
+    50% {
+      -webkit-transform: scale(1.1);
+      transform: scale(1.1);
+    }
+    100% {
+      -webkit-transform: scale(1);
+      transform: scale(1);
+    }
+  }
+  @keyframes pulsate-fwd {
+    0% {
+      -webkit-transform: scale(1);
+      transform: scale(1);
+    }
+    50% {
+      -webkit-transform: scale(1.1);
+      transform: scale(1.1);
+    }
+    100% {
+      -webkit-transform: scale(1);
+      transform: scale(1);
+    }
+  }
+`;
+
+export const Live = () => {
+  return (
+    <StyledContainer>
+      <CheckCircle fill={"currentColor"} />
+      Live
+    </StyledContainer>
+  );
+};
