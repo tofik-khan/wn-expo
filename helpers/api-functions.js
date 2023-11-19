2d3e54907f0209c59936f176cb6956ea2d2ebb8f
Tofik Khan <tofik.khan@mkausa.org>
2023-11-19 13:53:13 -0800

    Create API route & helpers for getSessions

---
+++ helpers/api-functions.js
@@ -0,0 +1,19 @@
+import * as INDEX from "../constants/session-index";
+export const processSessionData = (data) => {
+  //split title row from data
+  const sessions = data.splice(1);
+  const returnArray = sessions.map((session) => {
+    return {
+      title: session[INDEX.TITLE],
+      description: session[INDEX.DESCRIPTION],
+      thumbnail: session[INDEX.THUMBNAIL],
+      date: session[INDEX.DATE],
+      startTime: session[INDEX.STARTTIME],
+      endTime: session[INDEX.ENDTIME],
+      link: session[INDEX.LINK],
+      presenters: JSON.parse(session[INDEX.PRESENTERS]),
+    };
+  });
+
+  return returnArray;
+};
