26d87bb79c797aa7817dfff93d155e97c43c406b
Tofik Khan <tofik.khan@mkausa.org>
2023-11-19 13:54:58 -0800

    Create page for showing session/agenda

---
+++ pages/agenda.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import { AgendaCard } from "../components/adenda-card";
+import { InfoModal } from "../components/info-modal";
+import { processSessionData } from "../helpers/api-functions";
+
+const Page = () => {
+  const [sessions, updateSessions] = useState([]);
+  const [loaded, updateLoaded] = useState(false);
+  const [showModal, updateShowModal] = useState(false);
+  const [modalBody, updateModalBody] = useState({});
+
+  useEffect(() => {
+    fetch("/api/get-sessions")
+      .then((response) => response.json())
+      .then((response) => updateSessions(processSessionData(response.data)));
+    updateLoaded(true);
+  }, []);
+
+  if (!loaded) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <>
+      {sessions.map((session, index) => (
+        <AgendaCard
+          key={`session-id-${index}`}
+          title={session.title}
+          description={session.description}
+          thumbnail={session.thumbnail}
+          date={session.date}
+          startTime={session.startTime}
+          endTime={session.endTime}
+          presenters={session.presenters}
+          onClick={() => {
+            updateModalBody(session);
+            updateShowModal(true);
+          }}
+        />
+      ))}
+      <InfoModal
+        show={showModal}
+        onHide={() => updateShowModal(false)}
+        content={modalBody}
+      />
+    </>
+  );
+};
+
+export default Page;
