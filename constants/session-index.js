234090e108f9a4dc3eb4637b44c94c5359a280be
Tofik Khan <tofik.khan@mkausa.org>
2023-11-19 13:54:17 -0800

    Create Constants file for mapping API data

---
+++ constants/session-index.js
@@ -0,0 +1,8 @@
+export const TITLE = 0;
+export const DESCRIPTION = 1;
+export const THUMBNAIL = 2;
+export const DATE = 3;
+export const STARTTIME = 4;
+export const ENDTIME = 5;
+export const LINK = 6;
+export const PRESENTERS = 7;
