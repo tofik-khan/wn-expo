f75efa894c63b19fb5c80deb0dc9a7d11aedb9de
Tofik Khan <tofik.khan@mkausa.org>
2023-12-01 23:34:00 -0800

    Include InfoComponent to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Navigation } from "../partials/Nav";
 import { Hero } from "../partials/homepage/hero";
+import { InfoContainer } from "../partials/homepage/info";
 
 export const Page = () => {
   return (
     <>
       <Navigation />
       <Hero />
+      <InfoContainer />
     </>
   );
 };
