8d329477642d585a88ec23d8fa8a350159617beb
Tofik Khan <tofik.khan@mkausa.org>
2023-12-01 22:00:55 -0800

    Create WhiteText component

--- components/Text.js
+++ components/Text.js
@@ -14,3 +14,18 @@ export const LightText = styled.p`
     }
   }};
 `;
+
+export const WhiteText = styled.p`
+  color: white;
+  font-size: ${(props) => {
+    switch (props.size) {
+      case "lg":
+        return "20px";
+      case "md":
+        return "16px";
+      case "small":
+      default:
+        return "12px";
+    }
+  }};
+`;
