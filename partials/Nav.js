306751cde7ebb0bf3e4d8106efc8df8b5c2bd30e
Tofik Khan <tofik.khan@mkausa.org>
2024-01-15 18:09:54 -0800

    Include Head element in Nav

--- partials/Nav.js
+++ partials/Nav.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import Image from "next/image";
+import Head from "next/head";
 import Hamburger from "hamburger-react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import ExpoLogo from "/public/images/expo-logo.png";
@@ -44,6 +45,29 @@ export const Navigation = () => {
 
   return (
     <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <StyledNavBar fixed="top" variant="dark" expand="lg">
         <Container>
           <Navbar.Brand href="/">
