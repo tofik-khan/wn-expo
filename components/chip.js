45176d328e7076dfbd27b9f8d6b481121b36ca34
Tofik Khan <tofik.khan@mkausa.org>
2024-01-15 16:40:56 -0800

    Create Chip Component

---
+++ components/chip.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Chip = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 4px;
+
+  max-width: fit-content;
+
+  padding: 4px 8px;
+  border: 1px solid #e3e7e5;
+  border-radius: 4px;
+
+  color: #6e7573;
+`;
