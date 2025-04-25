99399d47b33bbfc892e416aeb67bdc07b1acdefc
Tofik Khan <tofik.khan@mkausa.org>
2025-04-25 16:01:17 -0700

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { Button, Typography } from "@mui/material";
 import AppBar from "@/components/AppBar";
 
 const App = () => {
-  const date = new Date().getFullYear();
   return (
     <>
       <title>Waqf-e-Nau Expo</title>
