7878a1bf8ff6a3860f9c53728e948ba6f8820c1b
Tofik Khan <tofik.khan@mkausa.org>
2025-04-27 15:37:43 -0700

    Add Query Provider to app

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import App from './App.tsx'
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme/mainTheme.ts";
 import { AuthProvider } from "./AuthProvider.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <AuthProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </AuthProvider>
     </ThemeProvider>
   </StrictMode>
