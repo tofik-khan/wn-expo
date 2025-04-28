3457a5835f8c25ae2860f774bef558ebc8497762
Tofik Khan <tofik.khan@mkausa.org>
2025-04-27 23:10:26 -0700

    Create a 403 page

--- src/pages/403.tsx
+++ src/pages/403.tsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
+import { DoNotDisturb } from "@mui/icons-material";
 
 export const Page403 = () => {
   const { logout } = useAuth0();
@@ -12,8 +13,26 @@ export const Page403 = () => {
   };
   return (
     <>
-      <p>UnAuthorized Login</p>
-      <Button onClick={handleLogout}>Logout</Button>
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          height: "80vh",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <DoNotDisturb sx={{ width: "200px", height: "200px" }} color="error" />
+        <Typography variant="h1" color="error">
+          Error 403
+        </Typography>
+        <Typography variant="subtitle1">
+          You do not have access to this section
+        </Typography>
+        <Button sx={{ mt: 3 }} variant="contained" onClick={handleLogout}>
+          Logout
+        </Button>
+      </Container>
     </>
-  )
-}
+  );
+};
