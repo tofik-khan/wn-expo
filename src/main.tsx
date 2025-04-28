4b8224caaf0506fa4ee47c21f09513612e28c271
Tofik Khan <tofik.khan@mkausa.org>
2025-04-27 23:55:25 -0700

    Add Redux Provider to app

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme/mainTheme.ts";
 import { AuthProvider } from "./AuthProvider.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import store from "./store.ts";
 
 const queryClient = new QueryClient();
 
@@ -13,7 +15,9 @@ createRoot(document.getElementById("root")!).render(
     <ThemeProvider theme={theme}>
       <AuthProvider>
         <QueryClientProvider client={queryClient}>
-          <App />
+          <Provider store={store}>
+            <App />
+          </Provider>
         </QueryClientProvider>
       </AuthProvider>
     </ThemeProvider>
