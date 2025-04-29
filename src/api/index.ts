8e399d34fa52508ba86c38a7b38cebaf66f957a9
Tofik Khan <tofik.khan@mkausa.org>
2025-04-28 18:16:25 -0700

    Add sessions and presenters to API

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,9 @@
 import { adminAPI } from '@/api/admin';
+import { sessionAPI } from "@/api/session";
+import { presentersAPI } from "@/api/presenters";
 
 export const API = {
   ...adminAPI,
-}
+  ...sessionAPI,
+  ...presentersAPI,
+};
