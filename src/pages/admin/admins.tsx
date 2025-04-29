7a17cfb020ab4e19fab6857ccd51972f52a09912
Tofik Khan <tofik.khan@mkausa.org>
2025-04-28 17:16:44 -0700

    Create Admins section with simple data

--- src/pages/admin/admins.tsx
+++ src/pages/admin/admins.tsx
@@ -1,15 +1,81 @@
 import { useAdminsQuery } from "@/queries/admin"
-import { Typography } from "@mui/material"
+import { Avatar, Chip, Typography } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 export const PageAdmins = () => {
-  const { data: admins, isLoading: isLoadingAdmins} = useAdminsQuery()
+  const { data: admins, isLoading: isLoadingAdmins } = useAdminsQuery();
 
-  if(isLoadingAdmins) return <p>Loading...</p>
-  
+  const columns: GridColDef[] = [
+    {
+      field: "index",
+      headerName: "#",
+      renderCell: ({ row }) => row.index,
+      width: 50,
+      headerAlign: "center",
+      align: "center",
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      renderCell: ({ row }) => (
+        <Chip
+          avatar={<Avatar alt={row.name} src={row.image} />}
+          label={row.name}
+          variant="outlined"
+        />
+      ),
+      flex: 3,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      renderCell: ({ row }) => row.email,
+      flex: 4,
+    },
+    {
+      field: "title",
+      headerName: "Title",
+      renderCell: ({ row }) => row.title,
+      flex: 3,
+    },
+    {
+      field: "isSuperuser",
+      headerName: "Role",
+      renderCell: ({ row }) =>
+        row.isSuperuser ? (
+          <Chip label={"Super User"} color="primary" />
+        ) : (
+          <Chip label={"Admin"} color="secondary" />
+        ),
+      flex: 2,
+    },
+    {
+      field: "isAuthorized",
+      headerName: "",
+      renderCell: ({ row }) =>
+        row.isAuthorized ? (
+          <Chip label={"Authorized"} color="success" variant="outlined" />
+        ) : (
+          <Chip label={"Unauthorized"} color="error" variant="outlined" />
+        ),
+      flex: 2,
+    },
+    {
+      field: "actions",
+      headerName: "",
+      flex: 3,
+    },
+  ];
+
   return (
     <>
       <Typography variant="h2">Admins:</Typography>
-      {JSON.stringify(admins)}
+      <DataGrid
+        loading={isLoadingAdmins}
+        rows={admins.map((admin, index) => ({ ...admin, index }))}
+        columns={columns}
+        getRowId={(row) => row._id}
+      />
     </>
-  )
-}
+  );
+};
