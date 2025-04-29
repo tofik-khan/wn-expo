da0da279f0fc6cba4c7510bd89342ed620d24a94
Tofik Khan <tofik.khan@mkausa.org>
2025-04-28 17:33:46 -0700

    Add env variable for api selection

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,1 +1,4 @@
-export const API_BASE = "https://wn-expo-backend.vercel.app";
+export const API_BASE =
+  import.meta.env.ENV === "DEV"
+    ? "http://localhost:3000"
+    : "https://wn-expo-backend.vercel.app";
