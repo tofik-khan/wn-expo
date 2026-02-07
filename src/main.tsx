eaec75256a93df52807624f70326fc8a83fdfa9b
Tofik Khan <tofik.khan@mkausa.org>
2026-02-06 20:28:07 -0800

    Update main to remove Auth Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from './App.tsx'
+import App from "./App.tsx";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme/mainTheme.ts";
-import { AuthProvider } from "./AuthProvider.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import store from "./store.ts";
 import { LicenseInfo } from "@mui/x-license";
+import "./style.css";
 
 LicenseInfo.setLicenseKey(import.meta.env.VITE_MUI_X_PRO_LICENSE);
 
@@ -16,13 +16,11 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <AuthProvider>
-        <QueryClientProvider client={queryClient}>
-          <Provider store={store}>
-            <App />
-          </Provider>
-        </QueryClientProvider>
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </QueryClientProvider>
     </ThemeProvider>
-  </StrictMode>
+  </StrictMode>,
 );
