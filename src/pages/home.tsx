5d4a4748167f7d94e547054e8f85e633c592985a
Tofik Khan <tofik.khan@mkausa.org>
2026-02-06 21:34:30 -0800

    Create Partners & Sponsors section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,7 @@
 import { SectionCTA } from "@/components/homepage/sections/CTA";
 import { SectionHero } from "@/components/homepage/sections/hero";
 import { SectionInfo } from "@/components/homepage/sections/info";
+import { SectionPartners } from "@/components/homepage/sections/partners";
 import { SectionVenue } from "@/components/homepage/sections/venue";
 import { Box } from "@mui/material";
 
@@ -12,6 +13,7 @@ export const PageHome = () => {
         <SectionCTA />
         <SectionInfo />
         <SectionVenue />
+        <SectionPartners />
       </Box>
     </>
   );
