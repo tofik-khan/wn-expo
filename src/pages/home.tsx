e6ac78288770f7e97a239eb3a62c17e7c00d4d96
Tofik Khan <tofik.khan@mkausa.org>
2026-02-06 20:54:20 -0800

    Add Info elements

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,6 @@
 import { SectionCTA } from "@/components/homepage/sections/CTA";
 import { SectionHero } from "@/components/homepage/sections/hero";
+import { SectionInfo } from "@/components/homepage/sections/info";
 import { Box } from "@mui/material";
 
 export const PageHome = () => {
@@ -8,6 +9,7 @@ export const PageHome = () => {
       <Box>
         <SectionHero />
         <SectionCTA />
+        <SectionInfo />
       </Box>
     </>
   );
