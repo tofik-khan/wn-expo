9e9a1faacc10980b8df15c8880b9de36d2b918c6
Tofik Khan <tofik.khan@mkausa.org>
2026-02-06 21:12:46 -0800

    Add Venue info to homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,7 @@
 import { SectionCTA } from "@/components/homepage/sections/CTA";
 import { SectionHero } from "@/components/homepage/sections/hero";
 import { SectionInfo } from "@/components/homepage/sections/info";
+import { SectionVenue } from "@/components/homepage/sections/venue";
 import { Box } from "@mui/material";
 
 export const PageHome = () => {
@@ -10,6 +11,7 @@ export const PageHome = () => {
         <SectionHero />
         <SectionCTA />
         <SectionInfo />
+        <SectionVenue />
       </Box>
     </>
   );
