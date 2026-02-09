f7984ca36b145b213646190befe8b1a718ff709f
Tofik Khan <tofik.khan@mkausa.org>
2026-02-09 01:58:15 -0800

    Creat Types for Expo

---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+import { Dayjs } from "dayjs";
+
+export type Presenter = {
+  _id: string;
+  name: string;
+  department: string;
+  isFeatured: boolean;
+  image: string;
+  bio: string;
+};
+
+export type Session<TPresenter = { label: string; value: string }> = {
+  _id: string;
+  title: string;
+  description: string;
+  location: string;
+  startTime: Dayjs;
+  endTime: Dayjs;
+  date: string;
+  presenters: TPresenter[];
+  link: string;
+  thumbnail: string;
+  isPublished: boolean;
+};
+
+export type HourSession = {
+  _id: string;
+  sessions: Session<Presenter>[];
+};
