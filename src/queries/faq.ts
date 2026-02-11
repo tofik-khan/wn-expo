cf5ee0c9d54af6ee6c2a647b58559135e61076d5
Tofik Khan <tofik.khan@mkausa.org>
2026-02-11 01:02:15 -0800

    Update FAQ api and query

--- src/queries/faq.ts
+++ src/queries/faq.ts
@@ -1,29 +1,9 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { API } from "@/api";
-import { Faq } from "@/types/faq";
-import { useAppDispatch } from "@/hooks";
-import { showSuccess } from "@/reducers";
 
-export const useFaqMutation = () => {
-  const queryClient = useQueryClient();
-  const dispatch = useAppDispatch();
-  return useMutation({
-    mutationFn: ({authToken, data}: {authToken: string; data: Faq}) => 
-      API.createFaq({
-        authToken,
-        data
-      }),
-      onSuccess: () => {
-        queryClient.invalidateQueries({
-          queryKey: ["faqs"]
-        });
-        dispatch(showSuccess({open: true, message: "FAQ Created!"}))
-      }
-  })
-}
-
-export const useFaqQuery = () => useQuery({
+export const useFaqQuery = () =>
+  useQuery({
     queryKey: ["faqs"],
     queryFn: API.getFaqs,
-    select: (response) => response.data
+    select: (response) => response.data,
   });
