9cb5064a251c0a37d2907490a2892c4af60de07e
Tofik Khan <tofik.khan@mkausa.org>
2026-02-11 20:38:32 -0800

    FEAT: Create Empty state for presenters

--- src/pages/presenters.tsx
+++ src/pages/presenters.tsx
@@ -3,17 +3,30 @@ import { FeaturedPresenter } from "@/components/presenters/featured-presenter";
 import { PresenterModal } from "@/components/presenters/presenter-modal";
 import { SessionPresenter } from "@/components/presenters/session-presenter";
 import { usePresentersQuery } from "@/queries/presenters";
-import { Container, Divider, Grid, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
+import { useNavigate } from "react-router";
 
 export const PagePresenters = () => {
   const { data, isLoading } = usePresentersQuery();
   const [presenter, setPresenter] = useState(null);
+  const navigate = useNavigate();
 
   if (isLoading) return <Loading />;
 
-  const featuredPresenters = data.filter((presenter) => presenter.isFeatured);
-  const sessionPresenters = data.filter((presenter) => !presenter.isFeatured);
+  const featuredPresenters = data.filter(
+    (presenter) => presenter.isFeatured && presenter.isPublished,
+  );
+  const sessionPresenters = data.filter(
+    (presenter) => !presenter.isFeatured && presenter.isPublished,
+  );
 
   return (
     <>
@@ -30,6 +43,7 @@ export const PagePresenters = () => {
           Meet the speakers who will be leading the sessions at the Waqf-e-Nau
           EXPO
         </Typography>
+        {/* Featured Presenters */}
         {featuredPresenters.length > 0 && (
           <>
             <Divider sx={{ maxWidth: "400px", margin: "auto", my: 5 }}>
@@ -48,23 +62,65 @@ export const PagePresenters = () => {
           </>
         )}
 
-        <Divider sx={{ maxWidth: "400px", margin: "auto", my: 5 }}>
-          <Typography variant="h6">Session Speakers</Typography>
-        </Divider>
-        <Grid
-          container
-          justifyContent={{ md: "flex-start", xs: "center" }}
-          spacing={2}
-        >
-          {sessionPresenters.map((presenter) => (
-            <Grid size={{ xs: 12, md: 6, lg: 3 }} key={presenter._id}>
-              <SessionPresenter
-                onClick={() => setPresenter(presenter)}
-                presenter={presenter}
-              />
+        {/* Session Presetners */}
+        {sessionPresenters.length > 0 && (
+          <>
+            <Divider sx={{ maxWidth: "400px", margin: "auto", my: 5 }}>
+              <Typography variant="h6">Session Speakers</Typography>
+            </Divider>
+            <Grid
+              container
+              justifyContent={{ md: "flex-start", xs: "center" }}
+              spacing={2}
+            >
+              {sessionPresenters.map((presenter) => (
+                <Grid size={{ xs: 12, md: 6, lg: 3 }} key={presenter._id}>
+                  <SessionPresenter
+                    onClick={() => setPresenter(presenter)}
+                    presenter={presenter}
+                  />
+                </Grid>
+              ))}
             </Grid>
-          ))}
-        </Grid>
+          </>
+        )}
+
+        {/* Empty State */}
+        {featuredPresenters.length === 0 && sessionPresenters.length === 0 && (
+          <>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: 3,
+              }}
+            >
+              <img
+                src="/images/graphics/badge.png"
+                style={{ marginBlock: 2, width: "400px" }}
+              />
+
+              <Typography fontSize={"28px"} color="text.secondary">
+                No Presenters available yet.
+              </Typography>
+              <Box>
+                <Typography>You will learn about the speakers here.</Typography>
+                <Typography>
+                  Please check back later for the list of our presenters.
+                </Typography>
+              </Box>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => navigate("/")}
+              >
+                Return Home
+              </Button>
+            </Box>
+          </>
+        )}
       </Container>
       <PresenterModal
         open={presenter !== null}
